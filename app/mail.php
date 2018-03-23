b352c155660497721244b8ccc0ea44f4196d1d68
Nurbol Sarsenbaev <nurbol.proger@gmail.com>
2018-03-23 14:40:21 +0600

    fixed

--- app/mail.php
+++ app/mail.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -6,6 +7,21 @@ $project_name = "DreamMebel";
 $admin_email  = "info@dream-mebel.kz, client@marketing-time.kz";
 $server_mail = "<info@dream-mebel.kz>";
 $form_subject = $_POST['info'];
+
+$phone = $_POST['Телефон'];
+$email = $_POST['email'];
+
+if (isset($phone) && $phone != "") {
+	$_SESSION['Телефон'] = $phone;
+} else {
+	$_POST['Телефон'] = $_SESSION['Телефон'];
+}
+
+if (isset($email) && $email != "") {
+	$_SESSION['email'] = $email;
+} else {
+	$_POST['email'] = $_SESSION['email'];
+}
 
 //Script Foreach
 $c = true;
@@ -26,7 +42,6 @@ if ( $method === 'POST' ) {
 $message = "<table style='width: 100%;'>$message</table>";
 
 function adopt($text) {
-	// return '=?UTF-8?B?'.$text.'?=';
 	return '=?UTF-8?B?'.Base64_encode($text).'?=';
 }
 
@@ -39,35 +54,56 @@ mail($admin_email, adopt($form_subject), $message, $headers);
 
 switch ($_POST['info']) {
 	case 'Получить прайс-лист':
-		sendFile("Прайс-лист", "ПРАЙС ЛИСТ.pdf");
+		// sendFile("Прайс-лист", "ПРАЙС ЛИСТ.pdf");
+		$file_subject = "Прайс-лист";
+		$file_name = "ПРАЙС ЛИСТ.pdf";
+		$file_content = chunk_split(base64_encode(file_get_contents('./files/'.$file_name)));
+		$uid = md5(uniqid(time()));
+		
+		$file_header = "From: ".$project_name." ".$server_mail."\r\n";
+		$file_header .= "Reply-To: ".$admin_email."\r\n";
+		$file_header .= "MIME-Version: 1.0\r\n";
+		$file_header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";					
+		
+		// message & attachment
+		$file_message = "--".$uid."\r\n";
+		$file_message .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+		$file_message .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+		$file_message .= "--".$uid."\r\n";
+		$file_message .= "Content-Type: application/octet-stream; name=\"".$file_name."\"\r\n";
+		$file_message .= "Content-Transfer-Encoding: base64\r\n";
+		$file_message .= "Content-Disposition: attachment; filename=\"".$file_name."\"\r\n\r\n";
+		$file_message .= $file_content."\r\n\r\n";
+		$file_message .= "--".$uid."--";
+
+		mail($_POST['email'], adopt($file_subject), $file_message, $file_header);
 		break;
 	case 'Получить каталог':
-		sendFile("Каталог", "КАТАЛОГ.pdf");
+		// sendFile("Каталог", "КАТАЛОГ.pdf");
+		$file_subject = "Каталог";
+		$file_name = "КАТАЛОГ.pdf";
+		$file_content = chunk_split(base64_encode(file_get_contents('./files/'.$file_name)));
+		$uid = md5(uniqid(time()));
+		
+		$file_header = "From: ".$project_name." ".$server_mail."\r\n";
+		$file_header .= "Reply-To: ".$admin_email."\r\n";
+		$file_header .= "MIME-Version: 1.0\r\n";
+		$file_header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";					
+		
+		// message & attachment
+		$file_message = "--".$uid."\r\n";
+		$file_message .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+		$file_message .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+		$file_message .= "--".$uid."\r\n";
+		$file_message .= "Content-Type: application/octet-stream; name=\"".$file_name."\"\r\n";
+		$file_message .= "Content-Transfer-Encoding: base64\r\n";
+		$file_message .= "Content-Disposition: attachment; filename=\"".$file_name."\"\r\n\r\n";
+		$file_message .= $file_content."\r\n\r\n";
+		$file_message .= "--".$uid."--";
+
+		mail($_POST['email'], adopt($file_subject), $file_message, $file_header);
+
 		break;
 	default:
 		break;
-}
-
-function sendFile($file_subject, $file_name) {
-	$file_content = chunk_split(base64_encode(file_get_contents('./files/'.$file_name)));
-	$uid = md5(uniqid(time()));
-	
-	// header
-	$file_header = "From: ".$project_name.' '.$server_mail."\r\n";
-	$file_header .= "Reply-To: ".$admin_email."\r\n";
-	$file_header .= "MIME-Version: 1.0\r\n";
-	$file_header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
-	
-	// message & attachment
-	$file_message = "--".$uid."\r\n";
-	$file_message .= "Content-type:text/plain; charset=iso-8859-1\r\n";
-	$file_message .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-	$file_message .= "--".$uid."\r\n";
-	$file_message .= "Content-Type: application/octet-stream; name=\"".$file_name."\"\r\n";
-	$file_message .= "Content-Transfer-Encoding: base64\r\n";
-	$file_message .= "Content-Disposition: attachment; filename=\"".$file_name."\"\r\n\r\n";
-	$file_message .= $file_content."\r\n\r\n";
-	$file_message .= "--".$uid."--";
-
-	mail($_POST['email'], $file_subject, $file_message, $file_header);
 }
